0b218e63c01b1dd3c479002e08520a39a1854d1a
Leo Fabregoule <leofabregoule@gmail.com>
2024-09-06 02:04:28 +0200

    added makefile and management for more than 1 argc

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::string input;
+    char c;
+
+
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int j = 1; j < argc; j++)
+    {
+        input = argv[j];
+        for(std::size_t i = 0; i < input.length(); i++)
+        {
+            c = toupper(input[i]);
+            std::cout << c;
+        }
+    }
+    return (0);
+}
