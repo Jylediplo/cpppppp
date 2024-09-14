74eb3c0996e94d4d04bbebcb774c5e3580790694
jy jy <you@example.com>
2024-09-14 20:33:46 +0200

    started CPP03

---
+++ CPP04/animal.hpp
@@ -0,0 +1,36 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include <iostream>
+
+class Animal
+{
+	protected:
+	std::string type;
+	public:
+	Animal(): type("unknown")
+	{
+		std::cout << "An animal is born !" << std::endl;
+	}
+};
+
+class Dog: public Animal
+{
+	public:
+	Dog(std::string type)
+	{
+		this->type = type;
+		std::cout << "A dog is born" << std::endl;
+	}
+	void get_type()
+	{
+		std::cout << this->type << std::endl;
+	}
+};
+
+class Cat: public Animal
+{
+
+};
+
+#endif
---
+++ CPP04/main.cpp
@@ -0,0 +1,10 @@
+#include "animal.hpp"
+
+int main(void)
+{
+	Dog g("DOGG");
+	Cat h;
+
+	g.get_type();
+	return(0);
+}
