3de09183b473d13a0f7f9f69bc0949a0a0843f9b
jy jy <leofabregoule@gmail.com>
2024-10-05 10:30:07 +0200

    completeted ex00

---
+++ t.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "enter value ";
+	int str;
+
+	std::cin >> str;
+	if (!std::cin)
+		printf("not a number");
+	std::cout << std::endl << "str : " << str << std::endl;
+	// if (std::stoi(str) == 1)
+	// 	std::cout << "bien egal à 1"; 
+	return(0);
+}
