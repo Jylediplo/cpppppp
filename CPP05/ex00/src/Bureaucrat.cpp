519e7a062c268a5aa5649464b97419a215945521
JyLeDiplo <leofabregoule@gmail.com>
2025-03-04 13:39:47 +0100

    ex00 exceptions should be good

---
+++ CPP05/ex00/src/Bureaucrat.cpp
@@ -0,0 +1,74 @@
+#include "../headers/Bureaucrat.hpp"
+
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name), _grade(grade)
+{
+    checkGrade();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade)
+{
+    checkGrade();
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+    if (this != &other)
+    {
+        _grade = other._grade;
+        checkGrade();
+    }
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+const std::string &Bureaucrat::getName() const
+{
+    return _name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return _grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+    _grade--;
+    checkGrade();
+}
+
+void Bureaucrat::decrementGrade()
+{
+    _grade++;
+    checkGrade();
+}
+
+void Bureaucrat::checkGrade() const
+{
+    if (_grade < 1)
+    {
+        throw GradeTooHighException();
+    }
+    else if (_grade > 150)
+    {
+        throw GradeTooLowException();
+    }
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade is too high";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade is too low";
+}
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat)
+{
+    os << bureaucrat.getName() << " grade : " << bureaucrat.getGrade();
+    return os;
+}
---
+++ CPP05/ex00/src/main.cpp
@@ -0,0 +1,27 @@
+#include "../headers/Bureaucrat.hpp"
+
+
+int main()
+{
+    try
+    {
+        Bureaucrat b1("romain", 1);
+        std::cout << b1 << std::endl;
+
+        b1.decrementGrade();
+        std::cout << b1 << std::endl;
+
+        Bureaucrat b2("loic", 150);
+        std::cout << b2 << std::endl;
+
+        b2.incrementGrade();
+        std::cout << b2 << std::endl;
+
+        Bureaucrat b3("error", 151);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
+}
